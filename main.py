338f514d89ce9b9ffc6ad58b2ca1becdd25fe857
jockey520 <2251447784@qq.com>
2025-09-09 17:17:04 +0800

    重量转换程序

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #WeightConvert.py
-WeightStr = input("请输入带符号的重量值:")
+WeightStr = input("")
 if WeightStr[-2:] in ["kg"]:
     pd = (eval(WeightStr[0:-2])) * 2.2046
-    print("转换后的重量值是{:.3f}磅".format(pd))
+    print("对应的英制重量为{:.3f}磅".format(pd))
 elif WeightStr[-2:] in ["pd"]:
     kg = (eval(WeightStr[0:-2])) / 2.2046
-    print("转换后的重量值是{:.3f}公斤".format(kg))
+    print("对应的公制重量为{:.3f}公斤".format(kg))
 else:
     print("输入格式错误")
