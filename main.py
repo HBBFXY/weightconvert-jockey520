52050c8a1791c118a3fc92f36f8fe18484047abc
jockey520 <2251447784@qq.com>
2025-09-09 17:19:33 +0800

    重量

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-#WeightConvert.py
-WeightStr = input("")
-if WeightStr[-2:] in ["kg"]:
-    pd = (eval(WeightStr[0:-2])) * 2.2046
-    print("对应的英制重量为{:.3f}磅".format(pd))
-elif WeightStr[-2:] in ["pd"]:
-    kg = (eval(WeightStr[0:-2])) / 2.2046
-    print("对应的公制重量为{:.3f}公斤".format(kg))
+weight_str = input()
+unit = weight_str[-2:]
+value = float(weight_str[:-2])
+
+if unit == "kg":
+    pounds = value * 2.2046
+    print(f"对应的英制重量为{pounds:.3f}磅")
+elif unit == "pd":
+    kilograms = value * 0.4535
+    print(f"对应的公制重量为{kilograms:.3f}公斤")
 else:
     print("输入格式错误")
